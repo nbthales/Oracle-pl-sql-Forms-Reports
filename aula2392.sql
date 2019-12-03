23d944360459eea7ef4dd2335fe0c219add6ed43
Thales Terra <nbthales@gmail.com>
2019-12-02 22:14:13 -0300

    DUAL e SYSDATA
    create aula 2392.sql

---
+++ aula2392.sql
@@ -0,0 +1,3 @@
+select to_char(sysdate, 'dd/mm/yyyy hh24:mi:ss') as Data_Hora from dual;
+select to_char(sysdate, 'dd/mon/yyyy hh24:mi:ss') as Data_Hora from dual;
+select to_char(sysdate, 'dd/month/yyyy hh24:mi:ss') as DatA_hora from dual;
