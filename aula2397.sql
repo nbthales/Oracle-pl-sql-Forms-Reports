3c2f29d4a64a1ff55c32a933b46ce671f18d7da3
Thales Terra <nbthales@gmail.com>
2019-12-07 18:21:47 -0300

    last day e months between
    create aula 2397.sql

---
+++ aula2397.sql
@@ -0,0 +1,4 @@
+select months_between('31/08/2009','15/06/2009') as diferenca from dual;
+
+select last_day('06/12/2019') as ultimo_dia_mes from dual;
+select last_day(sysdate) as ultimo_dia_mes from dual;
