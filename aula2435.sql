8f75d641cc19d2052346923456d6f9fe872a8193
Thales Terra <nbthales@gmail.com>
2019-12-26 21:28:28 -0300

    mais TRIGGER de validação
    create aula2435.sql

---
+++ aula2435.sql
@@ -0,0 +1,17 @@
+create or replace trigger controla_preco
+  before insert or update of est_precovenda on estoque
+  for each row
+begin
+  if :new.est_precovenda < 100 or :new.est_precovenda > 200 then
+    raise_application_error(-20001, 'Não foi possível gravar!');
+  else
+    dbms_output.put_line('Produto gravado com sucesso!');
+  end if;
+end controla_preco;
+
+select * from estoque;
+
+--command window
+SQL> execute grava_estoque(24,'Video Aula Apple','un',80,'19/06/2009');
+SQL> execute grava_estoque(24,'Video Aula Apple','un',200,'19/06/2009');
+SQL> execute grava_estoque(24,'Video Aula Apple','un',180,'19/06/2009');
