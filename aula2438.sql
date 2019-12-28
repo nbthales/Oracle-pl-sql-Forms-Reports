a3289b8aab9537a6358cacc60fba9c3b5b82a217
Thales Terra <nbthales@gmail.com>
2019-12-27 21:24:49 -0300

    TRIGGER Iniciando Controle de Horarios
    create aula 2438.sql

---
+++ aula2438.sql
@@ -0,0 +1,11 @@
+create or replace trigger controla_horario
+  before insert on estoque
+begin
+  if () then
+    
+  else
+    
+  end if;
+end controla_horario;
+
+select to_char(sysdate,'hh24:mi:ss') from dual;
