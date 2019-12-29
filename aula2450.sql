a8ebd105d796e04415f8eff49d52fb38caeb80bb
Thales Terra <nbthales@gmail.com>
2019-12-29 14:28:16 -0300

    FinalizandoTrigger com Updating Inserting e Deleting IF
    create aula2450.sql

---
+++ aula2450.sql
@@ -0,0 +1,24 @@
+create or replace trigger baixa_estoque
+  after insert or update or delete of ven_quantidade on venda
+  for each row
+begin
+  if inserting then
+    update estoque set est_quantidade = est_quantidade - :new.ven_quantidade
+    where est_codigo = :new.ven_codprod;
+  elsif deleting then
+    update estoque set est_quantidade = est_quantidade + :old.ven_quantidade
+    where est_codigo = :old.ven_codprod;
+  elsif updating then
+    update estoque set est_quantidade = est_quantidade + :old.ven_quantidade
+      where est_codigo = :old.ven_codprod;
+    update estoque set est_quantidade = est_quantidade - :new.ven_quantidade
+      where est_codigo = :new.ven_codprod;
+  end if;
+end baixa_estoque;
+
+select * from estoque;
+select * from venda;
+
+insert into venda (ven_codigo, ven_codprod, ven_quantidade) values(3, 4, 7);
+delete venda where ven_codigo = 3;
+update venda set ven_quantidade = 5 where ven_codigo = 3;
